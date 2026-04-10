cc994662248773b3fa8abeeb7766beb076ecc3ca
mercy <tomthomas212@gmail.com>
2026-04-10 15:14:55 -0400

    second message

--- src/test/java/testscript/TC_001Login.java
+++ src/test/java/testscript/TC_001Login.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class Login extends Base {
+public class TC_001Login extends Base {
 
 	@Test(priority=1)
 	public void loginWithValid()
