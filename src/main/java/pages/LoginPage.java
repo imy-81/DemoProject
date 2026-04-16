cfa235c495120eccc194ed2b9b4066ea9d8f4d23
mercy <tomthomas212@gmail.com>
2026-04-16 10:46:26 -0400

    third comment

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -6,37 +6,41 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
-	
-	
-	
-	
-	
-	@FindBy(xpath="//input[@placeholder='Username']")WebElement user;
-	@FindBy(xpath="//input[@placeholder='Password']")WebElement pswd;
+	//locators
+	@FindBy(xpath="//input[@placeholder='Username']")WebElement username;
+	@FindBy(xpath="//input[@placeholder='Password']")WebElement password;
 	@FindBy(xpath="//button[text()='Sign In']")WebElement signin;
+	
+	
+	
+	
 	public WebDriver driver;
+	
 	
 	public LoginPage(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
-
-
+	
+	//actions
+	
 	public void username(String usernamevalue)
 	{
-		user.sendKeys(usernamevalue);
+		
+		username.sendKeys(usernamevalue);
 	}
-	
 
 	public void pswrd(String passwordvalue)
 	{
-		pswd.sendKeys(passwordvalue);
+		password.sendKeys(passwordvalue);
 	}
 
-	public void signin()
+	public void clickOnSignin()
 	{
 		signin.click();
 	}
 	
 }
+	
+
+
--- src/test/java/testscript/LoginTest.java
+++ src/test/java/testscript/LoginTest.java
@@ -1,24 +1,77 @@
 package testscript;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
+import utilities.ExcelUtility;
 
 public class LoginTest extends Base {
 
-	@Test
-	public void verifyTheUserIsAbleToLoginUsingValidCred()
+	@Test(priority=1)
+	public void verifyTheUserIsAbleToLoginUsingValidCred() throws IOException
 	{
 		
-		String usernamevalue="admin";
-		String passwordvalue="admin";
+		//String usernamevalue="admin";
+		//String passwordvalue="admin";
+		
+		String usernamevalue=ExcelUtility.getStringData(1, 0, "loginpage");
+		
+		String passwordvalue=ExcelUtility.getStringData(1, 1,"loginpage");
 		
 		LoginPage loginpage=new LoginPage(driver);
 		loginpage.username(usernamevalue);
 		loginpage.pswrd(passwordvalue);
-		loginpage.signin();
+		loginpage.clickOnSignin();
 		
 	}
+	
+	
+	@Test(priority=2)
+	public void verifyTheUserIsAbleToLoginUsinginValidPswrd() throws IOException
+	{
+		
+		//String usernamevalue="admin";
+		//String passwordvalue="admin123";
+		String usernamevalue=ExcelUtility.getStringData(2, 0, "loginpage");
+		String passwordvalue=ExcelUtility.getStringData(2, 1, "loginpage");
+		LoginPage loginpage=new LoginPage(driver);
+		loginpage.username(usernamevalue);
+		loginpage.pswrd(passwordvalue);
+		loginpage.clickOnSignin();
+		
+	}
+	@Test(priority=3)
+	
+	public void verifyTheUserIsAbleToLoginUsinginValidUser() throws IOException
+			
+			{
+		//String usernamevalue="admin123";
+		//String passwordvalue="admin";
+		String usernamevalue=ExcelUtility.getStringData(3, 0, "loginpage");
+		String passwordvalue=ExcelUtility.getStringData(3, 1, "loginpage");
+		LoginPage loginpage=new LoginPage(driver);
+		loginpage.username(usernamevalue);
+		loginpage.pswrd(passwordvalue);
+		loginpage.clickOnSignin();
+			}	
+			
+@Test
+	
+	public void verifyTheUserIsAbleToLoginUsinginValidBoth() throws IOException
+			
+			{
+		//String usernamevalue="admin123";
+		//String passwordvalue="admin456";
+	String usernamevalue=ExcelUtility.getStringData(4, 0, "loginpage");
+	String passwordvalue=ExcelUtility.getStringData(4, 1, "loginpage");
+		LoginPage loginpage=new LoginPage(driver);
+		loginpage.username(usernamevalue);
+		loginpage.pswrd(passwordvalue);
+		loginpage.clickOnSignin();
+			}	
+
 }
